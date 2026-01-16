555ad424cc19d6a17e51b9ac474f0cdbfcac6a2e
tmsay1 <salihjaber01@gmail.com>
2026-01-16 14:47:28 +0300

    Create protection.py

---
+++ protection.py
@@ -0,0 +1,9 @@
+# protection.py
+
+def handle_message(message):
+    # هنا يمكنك إضافة الكود الذي يعالج الرسائل
+    pass
+
+def handle_member_update_roles(member):
+    # هنا يمكنك إضافة الكود الذي يعالج تحديث الأدوار
+    pass
